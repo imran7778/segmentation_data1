b7dc80c43322e540d6bd8b11f3e15fb09e87501f
imran7778 <31092568+imran7778@users.noreply.github.com>
2021-10-06 15:37:34 +0000

    new models

---
+++ model2.py
@@ -0,0 +1,27 @@
+def model():
+    inputs = Input(shape=(IMAGE_SIZE, IMAGE_SIZE, 3), name="input_image")
+    
+    encoder = MobileNetV2(input_tensor=inputs, weights="imagenet", include_top=False, alpha=0.35)
+    skip_connection_names = ["input_image", "block_1_expand_relu", "block_3_expand_relu", "block_6_expand_relu"]
+    encoder_output = encoder.get_layer("block_13_expand_relu").output
+    
+    f = [16, 32, 48, 64]
+    x = encoder_output
+    for i in range(1, len(skip_connection_names)+1, 1):
+        x_skip = encoder.get_layer(skip_connection_names[-i]).output
+        x = UpSampling2D((2, 2))(x)
+        x = Concatenate()([x, x_skip])
+        
+        x = Conv2D(f[-i], (3, 3), padding="same")(x)
+        x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        
+        x = Conv2D(f[-i], (3, 3), padding="same")(x)
+        x = BatchNormalization()(x)
+        x = Activation("relu")(x)
+        
+    x = Conv2D(1, (1, 1), padding="same")(x)
+    x = Activation("sigmoid")(x)
+    
+    model = Model(inputs, x)
+    return model
