c43fda4776115b4b9f5d5947b97570afb4e66c02
imran7778 <31092568+imran7778@users.noreply.github.com>
2021-10-28 14:51:24 +0000

    c1

--- all_params2.py
+++ all_params2.py
@@ -1,8 +1,8 @@
-TRAIN_DATA_PATH = 'SigTuple_data3/Train_data/'
-NEW_TRAIN_DATA_PATH = 'SigTuple_data3/New_train_data/'
-NEW_TEST_DATA_PATH = 'SigTuple_data3/New_test_data/'
-TEST_DATA_PATH = 'SigTuple_data3/Test_Data/'
-SUBMISSION_DATA_PATH = 'SigTuple_data3/Submission_Data/'
+TRAIN_DATA_PATH = 'SigTuple_data1/Train_data/'
+NEW_TRAIN_DATA_PATH = 'SigTuple_data1/New_train_data/'
+NEW_TEST_DATA_PATH = 'SigTuple_data1/New_test_data/'
+TEST_DATA_PATH = 'SigTuple_data1/Test_Data/'
+SUBMISSION_DATA_PATH = 'SigTuple_data1/Submission_Data/'
 MODEL_CHECKPOINT_DIR = 'Checkpoints/'
 WEIGHTS = 'Model_Weights.hdf5'
 AUGMENT_TRAIN_DATA = False
